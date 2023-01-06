1842f2610e0ad9fdaa945b01619101ae5634a68a
Aitor Arrieta <aarrieta@mondragon.edu>
2023-01-06 11:08:05 +0100

    New fix pattern added

--- Miscellanous/miscellanous.m
+++ Miscellanous/miscellanous.m
@@ -12,6 +12,8 @@ data = find(rt,'-isa','Stateflow.Data');
 inputs = getInputs(rt);
 outputs = getOutputs(rt);
 
+done = replaceConditionalOperator(transitions(2));
+
 done = insertVariableInState(states(1),outputs(1));
 hasIt = checkStateContainsVariable(states(1),outputs(1));
 variablesInState = getVariablesInState(getOutputs(rt),states(1));
@@ -24,6 +26,47 @@ done = numericalReplacementOfVariableInState(states(1),outputs(1));
 a = isInitialTransition(transitions(3));
 replaceInitialTransition(transitions,states);
 b = replacementOfTransitionSource(transitions(1),states);
+
+
+function done = replaceConditionalOperator(transition)
+    done = false;
+    replaceAnd = false;
+    replaceOr = false;
+    str = transition.LabelString;
+    if isInitialTransition(transition)==false
+       if contains(str,'&&')&& contains(str,'||')
+           if rand<=0.5
+               replaceAnd = true;
+           else
+               replaceOr = true;
+           end           
+       elseif contains(str,'&&')
+           replaceAnd = true;
+           
+       elseif contains(str,'||')
+           replaceOr = true;           
+       else
+          %No conditional operator 
+          
+       end
+       if replaceAnd
+            andLocations = strfind(str, '&&');
+            whichToChange = randi([1 length(andLocations)]);
+            str(andLocations(whichToChange)) = '|'; % Replace first e with null (in other words, delete it).
+            str(andLocations(whichToChange)+1) = '|';
+            transition.LabelString = str;
+            done = true;
+       end
+       if replaceOr
+            orLocations = strfind(str, '||');
+            whichToChange = randi([1 length(orLocations)]);
+            str(orLocations(whichToChange)) = '&'; % Replace first e with null (in other words, delete it).
+            str(orLocations(whichToChange)+1) = '&';
+            transition.LabelString = str;
+            done = true;
+       end
+    end
+end
 
 
 function done = insertVariableInState(state,output)
