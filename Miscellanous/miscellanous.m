b11cce479544358ed62d6c98002eb1eb46d169ba
Aitor Arrieta <aarrieta@mondragon.edu>
2023-01-09 17:17:58 +0100

    Added a new fix operator (replaceMathematicalOperatorInState)

--- Miscellanous/miscellanous.m
+++ Miscellanous/miscellanous.m
@@ -11,6 +11,7 @@ transitions = getTransitions(rt);%find(rt,'-isa','Stateflow.Transition');
 data = find(rt,'-isa','Stateflow.Data');
 inputs = getInputs(rt);
 outputs = getOutputs(rt);
+done = replaceMathematicalOperatorInState(states(1));
 
 done = replaceConditionalOperator(transitions(2));
 
@@ -26,6 +27,133 @@ done = numericalReplacementOfVariableInState(states(1),outputs(1));
 a = isInitialTransition(transitions(3));
 replaceInitialTransition(transitions,states);
 b = replacementOfTransitionSource(transitions(1),states);
+
+
+function done = replaceMathematicalOperatorInState(state)
+    done = false;
+    containsPlus = false;
+    containsMinus = false;
+    containsMult = false;
+    containsDiv = false;
+    plusLocations = 0;
+    minusLocations = 0;
+    multLocations = 0;
+    divLocations = 0;
+    str = convertCharsToStrings(state.Label);
+    if contains(str,'+') || contains(str,'-') ||...
+       contains(str,'*') || contains(str,'/')
+        if contains(str,'+')
+            containsPlus = true;
+            plusLocations = strfind(str, '+');
+        end
+        if contains(str,'-')
+            containsMinus = true;
+            minusLocations = strfind(str, '-');
+        end
+        if contains(str,'*')
+            containsMult = true;
+            multLocations = strfind(str, '*');
+        end
+        if contains(str,'/')
+            containsDiv = true;
+            divLocations = strfind(str, '/');
+        end
+        locations = [];
+        if containsPlus
+            locations = [locations  plusLocations];
+        end
+        if containsMinus
+            locations = [locations  minusLocations];
+        end
+        if containsMult
+            locations = [locations  multLocations];
+        end
+        if containsDiv
+            locations = [locations  divLocations];
+        end
+        whichToChange = randi([1 length(locations)]);
+        if strcmp(state.Label(locations(whichToChange)),'+')
+            if rand <= 0.5
+                state.Label(locations(whichToChange)) = '-';
+                done = true;
+            else
+               if rand <=0.5 && strcmp(state.Label(locations(whichToChange)+1), '0')==false
+                   state.Label(locations(whichToChange)) = '/';
+                   done = true;
+               else
+                   state.Label(locations(whichToChange)) = '*';
+                   done = true;
+               end
+            end
+            
+        elseif strcmp(state.Label(locations(whichToChange)),'-')
+            if rand <= 0.5
+                state.Label(locations(whichToChange)) = '+';
+                done = true;
+            else
+               if rand <=0.5 &&  strcmp(state.Label(locations(whichToChange)+1), '0')==false
+                   state.Label(locations(whichToChange)) = '/';
+                   done = true;
+               else
+                   state.Label(locations(whichToChange)) = '*';
+                   done = true;
+               end
+            end
+            
+        elseif strcmp(state.Label(locations(whichToChange)),'*')
+            if rand <= 0.5 && strcmp(state.Label(locations(whichToChange)+1), '0')==false
+                state.Label(locations(whichToChange)) = '/';
+                done = true;
+            else
+               if rand <=0.5
+                   state.Label(locations(whichToChange)) = '+';
+                   done = true;
+               else
+                   state.Label(locations(whichToChange)) = '-';
+                   done = true;
+               end
+            end
+            
+        elseif strcmp(state.Label(locations(whichToChange)),'/')
+            if rand <= 0.5
+                state.Label(locations(whichToChange)) = '*';
+                done = true;
+            else
+               if rand <=0.5
+                   state.Label(locations(whichToChange)) = '-';
+                   done = true;
+               else
+                   state.Label(locations(whichToChange)) = '+';
+                   done = true;
+               end
+            end
+            
+        end
+        
+%         numOfTotalOp = containsPlus+containsMinus+ContainsMult+containsDiv;
+%         prob = 1/numOfTotalOp;
+%         selectOp = randi([1,numOfTotalOp]);
+%         i=1;
+%         if containsPlus==1
+%             if selectOp == i
+%                %replace plus 
+%             end
+%             i = i+1;           
+%         end
+%         if containsMinus ==1
+%             if selectOp ==i
+%                 %replace minus
+%             end
+%         end
+%         if containsMult ==1
+%             if selectOp ==i
+%                 %replaceMult
+%             end
+%         end
+        
+    end
+    
+end
 
 
 function done = replaceConditionalOperator(transition)
