870e67b5aba4745997c79ebd92fe6279b3ccb35b
Aitor Arrieta <aarrieta@mondragon.edu>
2023-01-09 17:41:57 +0100

    Exended the replaceSecMsecInAfter to include usec too, with lower probability of being selected.

--- Miscellanous/miscellanous.m
+++ Miscellanous/miscellanous.m
@@ -31,21 +31,43 @@ b = replacementOfTransitionSource(transitions(1),states);
 function done = replaceSecMsecInAfter(transition)
     %TODO: All changes when more than one after in a transition. Could be
     %problematic, although this case is quite rare.
+    %TODO: Extend for use with "tick"? also, quite a rare change
     done = false;
     str = convertCharsToStrings(transition.LabelString);
     if isInitialTransition(transition)==false && contains(str,'after')
         secLocations = strfind(str, 'sec');
         whichToChange = randi([1 length(secLocations)]);
         if strcmp(transition.LabelString(secLocations(whichToChange)-1), 'm')
-            newStr = strrep(str,'msec','sec');
-            transition.LabelString = newStr;
-            done = true;
+            if rand<0.8
+                newStr = strrep(str,'msec','sec');
+                transition.LabelString = newStr;
+                done = true;
+            else
+                newStr = strrep(str,'msec','usec');
+                transition.LabelString = newStr;
+                done = true;
+            end
+        elseif strcmp(transition.LabelString(secLocations(whichToChange)-1), 'u')
+            if rand<0.8
+                newStr = strrep(str,'usec','msec');
+                transition.LabelString = newStr;
+                done = true;
+            else
+                newStr = strrep(str,'usec','sec');
+                transition.LabelString = newStr;
+                done = true;
+            end
         else
-            newStr = strrep(str,'sec','msec');
-            transition.LabelString = newStr;
-            done = true;
+            if rand < 0.8
+                newStr = strrep(str,'sec','msec');
+                transition.LabelString = newStr;
+                done = true;
+            else
+                newStr = strrep(str,'sec','usec');
+                transition.LabelString = newStr;
+                done = true;
+            end 
         end
-        
     end
 
 end
