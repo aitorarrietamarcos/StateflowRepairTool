a0b6b2e5aa29624e981548456dd4e13c4043cb0d
Aitor Arrieta <aarrieta@mondragon.edu>
2023-01-09 22:09:15 +0100

    Impleented a new mutation operator --> change of numerical value in a transition

--- Miscellanous/miscellanous.m
+++ Miscellanous/miscellanous.m
@@ -11,6 +11,7 @@ transitions = getTransitions(rt);%find(rt,'-isa','Stateflow.Transition');
 data = find(rt,'-isa','Stateflow.Data');
 inputs = getInputs(rt);
 outputs = getOutputs(rt);
+done = numericalChangeInTransition(transitions(2));
 done = relationalOperatorReplacement(transitions(2));
 
 done = replaceConditionalOperator(transitions(2));
@@ -27,6 +28,74 @@ done = numericalReplacementOfVariableInState(states(1),outputs(1));
 a = isInitialTransition(transitions(3));
 replaceInitialTransition(transitions,states);
 b = replacementOfTransitionSource(transitions(1),states);
+
+function done = numericalChangeInTransition(transition)
+    %TODO: Some times it may end a number to be --. Prune this.
+    %TODO: This implementation is much clearer than the numerical changes
+    %done in the states. Think how to integrate this in the states?
+    done = false;
+    str = convertCharsToStrings(transition.LabelString); 
+    numsOfStr = regexp(str,'\d*','Match');
+    whereNumsAre = [];
+    for ii=1:length(numsOfStr)
+        plusLocations = strfind(str,numsOfStr{ii});
+        for jj=1:length(plusLocations)
+            %isNum = isNumber(str,plusLocations(jj));
+            %aitor = 0;
+            if isNumber(str,plusLocations(jj))
+               whereNumsAre = [whereNumsAre plusLocations(jj)];
+            end
+        end
+       
+    end
+    whereNumsAre = unique(whereNumsAre);
+    selectedNum = whereNumsAre(randi([1 length(whereNumsAre)]));
+    chr = convertStringsToChars(str);
+    chrNew = chr(1:selectedNum-1);
+    ii=selectedNum;
+    while isstrprop(chr(ii),'digit')
+       ii=ii+1; 
+    end
+    
+    %TODO: Polish this. Especially, maximum number is -15 + 15. How to
+    %obtain another number?
+    if rand <=0.5
+        if rand <0.5
+            selectedNum = 1;
+        else
+            selectedNum = 0;
+        end
+    else
+        selectedNum = randi([-15 15]);
+    end
+    
+    chrNew = [chrNew num2str(selectedNum)];
+    chrNew = [chrNew chr(ii:length(chr))];
+    %
+    str2 = convertCharsToStrings(chrNew);
+    if strcmp(str,str2)==false
+        done = true;
+        transition.LabelString = str2;
+    end
+        
+end
+
+function isNum = isNumber(str,charNum)
+    isNum = false;
+    if charNum==1
+        isNum = true;
+    elseif charNum<1
+        isNum = false;
+    else
+        chr = convertStringsToChars(str);
+        if isletter(chr(charNum-1))||strcmp(chr(charNum-1),'_')
+            isNum=false;
+        else
+            isNum=true;
+        end
+    end
+
+end
 
 function done = relationalOperatorReplacement(transition)
     done = false;
